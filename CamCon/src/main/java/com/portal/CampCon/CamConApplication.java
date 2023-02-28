0cdf2c95fe4b751e90e5c521c2ca703f2db276ee
Nikhil <nikhilthorat1996@gmail.com>
2023-02-28 18:25:15 +0530

    Spring Boot Starter Project created

---
+++ CamCon/src/main/java/com/portal/CampCon/CamConApplication.java
@@ -0,0 +1,13 @@
+package com.portal.CampCon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamConApplication.class, args);
+	}
+
+}
